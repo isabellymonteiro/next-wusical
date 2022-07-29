a07ecb06a22e37d8d14d047e5e06171d789c0377
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-07-29 16:58:23 -0300

    project setup

---
+++ components/atoms/HelloWorld/index.jsx
@@ -0,0 +1,9 @@
+import styles from './HelloWorld.module.scss'
+
+const HelloWorld = () => {
+  return (
+    <h1 className={styles.helloWorld__title}>Hello world</h1>
+  )
+}
+
+export default HelloWorld
