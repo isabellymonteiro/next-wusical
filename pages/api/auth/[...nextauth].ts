01044eb8a85bab2ebd033272843d8ed3c208c8e5
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-08-10 23:23:42 -0300

    added try/catch to nextauth login logic

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,26 +11,30 @@ export default NextAuth({
     CredentialsProvider({
       credentials: {},
       async authorize(credentials: any) {
-        const client = await connectToDatabase()
+        try {
+          const client = await connectToDatabase()
 
-        const usersCollection = client.db().collection('users')
+          const usersCollection = client.db().collection('users')
 
-        const user = await usersCollection.findOne({ email: credentials?.email })
+          const user = await usersCollection.findOne({ email: credentials?.email })
 
-        if (!user) {
+          if (!user) {
+            client.close()
+            throw new Error('No user found.')
+          }
+
+          const isValid = await verifyPassword(credentials?.password, user.password)
+
+          if (!isValid) {
+            client.close()
+            throw new Error('Could not log you in.')
+          }
+
           client.close()
-          throw new Error('No user found.')
+          return { email: user.email } // will be encoded into json webtoken
+        } catch (e) {
+          return null
         }
-
-        const isValid = await verifyPassword(credentials?.password, user.password)
-
-        if (!isValid) {
-          client.close()
-          throw new Error('Could not log you in.')
-        }
-
-        client.close()
-        return { email: user.email } // will be encoded into json webtoken
       }
     })
   ]
