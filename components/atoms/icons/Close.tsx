dec74bc927deb95f471af196b0d0b24efd995cd1
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-08-13 16:12:53 -0300

    removed unused close component

--- components/atoms/icons/Close.tsx
+++
@@ -1,9 +0,0 @@
-const Close = () => {
-  return (
-    <svg width="27" height="25" viewBox="0 0 27 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M27 2.51786L24.2807 0L13.5 9.98214L2.71929 0L0 2.51786L10.7807 12.5L0 22.4821L2.71929 25L13.5 15.0179L24.2807 25L27 22.4821L16.2193 12.5L27 2.51786Z" fill="#061621"/>
-    </svg>
-  )
-}
-
-export default Close
